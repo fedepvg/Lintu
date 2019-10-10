57cb9c0fd45b29cafd7171d4b0dccbf8afe53870
fedepvg <federicopvg@gmail.com>
2019-10-10 12:52:27 -0300

    mill added

---
+++ Lintu/Assets/Scripts/Gameplay/Mill.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mill : MonoBehaviour
+{
+    public float RotSpeed;
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, RotSpeed * Time.deltaTime, 0f));
+    }
+}
