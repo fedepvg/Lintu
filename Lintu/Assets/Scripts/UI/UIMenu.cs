3f6fdfb06356cc6a8eac53a6eed4fea44b44dd58
fedepvg <federicopvg@gmail.com>
2019-10-14 11:47:38 -0300

    version text added

---
+++ Lintu/Assets/Scripts/UI/UIMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMenu : MonoBehaviour
+{
+    public Text VersionText;
+
+    void Start()
+    {
+        VersionText.text = "v" + Application.version;
+    }
+}
