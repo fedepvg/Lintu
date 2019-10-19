4d0a18facf350f957716be820570b11db8bbf2ab
fedepvg <federicopvg@gmail.com>
2019-10-19 17:44:39 -0300

    city building test scene created

---
+++ Lintu/Assets/Scripts/Gameplay/CityCreator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityCreator : MonoBehaviour
+{
+    #region PublicVariables
+    public enum Orientations { left, right };
+    public float Width;
+    public float Height;
+    public float Depth;
+    public float BorderDistance;
+    public Orientations Orientation;
+    public List<GameObject> Buildings;
+    #endregion
+
+
+    void Start()
+    {
+        transform.localScale = new Vector3(Width, Height, Depth);
+        CreateCity();
+    }
+
+    void CreateCity()
+    {
+
+    }
+}
