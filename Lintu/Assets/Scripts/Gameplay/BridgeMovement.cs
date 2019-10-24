490d2bd6391ab2c76490393a061727e5243117ab
fedepvg <federicopvg@gmail.com>
2019-10-24 12:51:54 -0300

    bridge movement added

--- Lintu/Assets/Scripts/Gameplay/BridgeMovement.cs
+++ Lintu/Assets/Scripts/Gameplay/BridgeMovement.cs
@@ -8,11 +8,19 @@ public class BridgeMovement : MonoBehaviour
     public GameObject RightPart;
     public float  RotSpeed;
 
-    float MaxRot;
-    int DistanceMultiplier;
+    float MaxRot = 15;
+    int LeftDirectionMultiplier = -1;
+    int RightDirectionMultiplier = 1;
 
     private void Update()
     {
-        
+        LeftPart.transform.Rotate(Vector3.forward * RotSpeed * LeftDirectionMultiplier * Time.deltaTime);
+        RightPart.transform.Rotate(Vector3.forward * RotSpeed * RightDirectionMultiplier * Time.deltaTime);
+
+        if (RightPart.transform.localRotation.eulerAngles.z >= MaxRot || RightPart.transform.localRotation.eulerAngles.z <= 0)
+            RightDirectionMultiplier *= -1;
+
+        if (LeftPart.transform.localRotation.eulerAngles.z <= 360 - MaxRot && LeftPart.transform.localRotation.eulerAngles.z > 0)
+            LeftDirectionMultiplier *= -1;
     }
 }
