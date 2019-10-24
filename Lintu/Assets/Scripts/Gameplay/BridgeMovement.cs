4b5e4ff666a35f60cd398e628e71807ebf8048c7
fedepvg <federicopvg@gmail.com>
2019-10-24 11:34:57 -0300

    bridge movement script created

---
+++ Lintu/Assets/Scripts/Gameplay/BridgeMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeMovement : MonoBehaviour
+{
+    public GameObject LeftPart;
+    public GameObject RightPart;
+    public float  RotSpeed;
+
+    float MaxRot;
+    int DistanceMultiplier;
+
+    private void Update()
+    {
+        
+    }
+}
