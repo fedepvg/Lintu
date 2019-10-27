cc20d4e7caa1fe54ee330b2e9de5c40584201f6e
fedepvg <federicopvg@gmail.com>
2019-10-27 12:23:57 -0300

    fake load scripts added

---
+++ Lintu/Assets/Scripts/Manager/LoaderManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoaderManager : MonoBehaviourSingleton<LoaderManager>
+{
+    public float LoadingProgress;
+    public bool FakeLoad;
+    public float TimeLoading;
+    public float MinTimeToLoad = 2;
+    public void LoadScene(string sceneName)
+    {
+        UILoadingScreen.Instance.SetVisible(true);
+        if (FakeLoad)
+        {
+            StartCoroutine(AsynchronousLoadWithFake(sceneName));  
+        }
+        else
+        {
+            StartCoroutine(AsynchronousLoad(sceneName));    
+        }
+    }
+
+    IEnumerator AsynchronousLoad(string scene)
+    {
+        LoadingProgress = 0;
+
+        yield return null;
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            LoadingProgress = ao.progress + 0.1f;
+
+            // Loading completed
+            if (ao.progress >= 0.9f)
+            {
+                ao.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+
+    IEnumerator AsynchronousLoadWithFake(string scene)
+    {
+        LoadingProgress = 0;
+        TimeLoading = 0;
+        yield return null;
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            TimeLoading += Time.deltaTime;
+            LoadingProgress = ao.progress + 0.1f;
+            LoadingProgress = LoadingProgress * TimeLoading / MinTimeToLoad;
+
+            // Loading completed
+            if (LoadingProgress >= 1)
+            {
+                ao.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Lintu/Assets/Scripts/UI/UILoadingScreen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.UI;
+
+public class UILoadingScreen : MonoBehaviourSingleton<UILoadingScreen>
+{
+    public Slider LoadingSlider;
+
+    public override void Awake()
+    {
+        base.Awake();
+        gameObject.SetActive(false);
+    }
+
+    public void SetVisible(bool show)
+    {
+        gameObject.SetActive(show);
+    }
+
+    public void Update()
+    {
+        int loadingVal = (int) (LoaderManager.Instance.LoadingProgress * 100);
+        LoadingSlider.value = loadingVal;
+        if (LoaderManager.Instance.LoadingProgress >= 1)
+            SetVisible(false);
+    }
+}
