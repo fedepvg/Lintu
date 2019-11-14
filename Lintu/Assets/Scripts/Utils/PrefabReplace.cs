8ff6751e6f8624bc1949f873c09553acd55a2a85
fedepvg <federicopvg@gmail.com>
2019-11-14 13:04:49 -0300

    some vigas replaced

--- Lintu/Assets/Scripts/Utils/PrefabReplace.cs
+++ Lintu/Assets/Scripts/Utils/PrefabReplace.cs
@@ -10,15 +10,17 @@ public class PrefabReplace : ScriptableObject
 
         // go over all selected objects. Change this if you want to go
         // through other objects, e.g. GameObject.FindWithTag ...
+        int i = 0;
         foreach (GameObject s in Selection.objects)
         {
             // create a new prefab
-            var go = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
-            go.name = s.name; // remember name
-            EditorUtility.CopySerialized(s.transform, go.transform); // remember position/rotation etc..
+            GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+            go.name = "Viga_" + i; // remember name
             go.transform.parent = s.transform.parent; // put into hierarchy
+            EditorUtility.CopySerialized(s.transform, go.transform); // remember position/rotation etc..
             PrefabUtility.RecordPrefabInstancePropertyModifications(go); // fix modified properties
             GameObject.DestroyImmediate(s); // destroy old
+            i++;
         }
     }
 }
