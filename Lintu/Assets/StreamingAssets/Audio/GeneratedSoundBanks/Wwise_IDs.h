2309bb969f7e912ad1278bd98a7cb085c8363107
fedepvg <federicopvg@gmail.com>
2019-11-19 16:11:12 -0300

    wwise project 1 added

---
+++ Lintu/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,59 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BARCOS_IDLE = 1876704714U;
+        static const AkUniqueID CLICK_MOUSE = 2013887443U;
+        static const AkUniqueID COLISION_AGUA = 3661761514U;
+        static const AkUniqueID COLISION_MADERA = 3999760164U;
+        static const AkUniqueID GANAR = 3575068712U;
+        static const AkUniqueID INICIO_JUEGO = 1460932951U;
+        static const AkUniqueID INICIO_MENU = 880580374U;
+        static const AkUniqueID INICIO_START = 194776941U;
+        static const AkUniqueID MENU_CREDITOS = 1063492266U;
+        static const AkUniqueID ORB_PICKUP = 3042702347U;
+        static const AkUniqueID PAJARO_ALETEA = 4246722315U;
+        static const AkUniqueID PAJARO_VOZ = 2577500974U;
+        static const AkUniqueID PAUSA_JUEGO = 1935209442U;
+        static const AkUniqueID PERDER = 2433235219U;
+        static const AkUniqueID RESTART = 1203400786U;
+        static const AkUniqueID VOLVER_A_MENU = 3332192915U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID ALTURA_PAJARO = 1768459312U;
+        static const AkUniqueID ENERGIA_PAJARO = 201235242U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
