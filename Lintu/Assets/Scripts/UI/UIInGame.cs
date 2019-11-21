4df7b5bfa16afcd0eba792acee080ec0e099d327
fedepvg <federicopvg@gmail.com>
2019-11-21 10:53:20 -0300

    level distance image added

--- Lintu/Assets/Scripts/UI/UIInGame.cs
+++ Lintu/Assets/Scripts/UI/UIInGame.cs
@@ -10,19 +10,27 @@ public class UIInGame : MonoBehaviour
     #region PublicVariables
     public Slider EnergyBar;
     public Image EnergyBarFill;
-    public Slider AltitudeBar;
+    public Slider DistanceBar;
     public BirdController Player;
     public GameObject PausePanel;
     public Transform StartTransform;
     public Transform FinishTransform;
+    public Sprite LintuUI1;
+    public Sprite LintuUI2;
+    public float DistanceHandleAnimationTime;
+    public Image DistanceBarHandle;
     #endregion
 
+    float SpriteChangeTimer;
     float LevelDistance;
+    Sprite CurrentDistanceHandleSprite;
 
     private void Start()
     {
         GameManager.Instance.GameInput.Gameplay.Pause.performed += ctx => SetPauseState();
         LevelDistance = FinishTransform.position.z - StartTransform.position.z;
+        SpriteChangeTimer = 0;
+        CurrentDistanceHandleSprite = LintuUI1;
     }
 
     void Update()
@@ -33,7 +41,18 @@ public class UIInGame : MonoBehaviour
         else
             EnergyBarFill.color = Color.white;
 
-        AltitudeBar.value = Player.LevelDistanceLeft * 100 / LevelDistance;
+        DistanceBar.value = Player.LevelDistanceLeft * 100 / LevelDistance;
+        SpriteChangeTimer += Time.deltaTime;
+        if(SpriteChangeTimer>=DistanceHandleAnimationTime)
+        {
+            SpriteChangeTimer = 0f;
+            if (CurrentDistanceHandleSprite == LintuUI1)
+                CurrentDistanceHandleSprite = LintuUI2;
+            else
+                CurrentDistanceHandleSprite = LintuUI1;
+            DistanceBarHandle.sprite = CurrentDistanceHandleSprite;
+        }
+
 
         if (!EventSystem.current.currentSelectedGameObject && PausePanel.activeSelf && GameManager.Instance.GameInput.UI.Navigate.triggered)
             EventSystem.current.SetSelectedGameObject(EventSystem.current.firstSelectedGameObject);
