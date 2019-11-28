4789c9fa2a3857bf1dd65cbe56638dfd94b195b7
fedepvg <federicopvg@gmail.com>
2019-11-28 09:49:46 -0300

    camera prefab modified

--- Lintu/Assets/Scripts/Gameplay/CameraController.cs
+++ Lintu/Assets/Scripts/Gameplay/CameraController.cs
@@ -5,29 +5,18 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float CameraYOffset;
-    public float CameraZOffset;
     public float CameraMaxYOffset;
-    public float CameraMiddleYOffset;
-    public float CameraMinYOffset;
     public Transform PlayerTransform;
     public float CameraYSpeedMultiplier;
-    public CameraPositions CameraPosition;
     public Vector3 CurrentVelocity;
     public float SmoothTime;
     public Vector3 Distance;
 
-    CameraPositions CameraLastposition;
     float CameraYSpeed;
     bool LevelEnded = false;
 
-    public enum CameraPositions
-    {
-        up, middle, down
-    }
-
     private void Start()
     {
-        CameraPosition = CameraPositions.middle;
         BirdController.EndLevelAction = StopMovingCamera;
 
     }
@@ -37,24 +26,7 @@ public class CameraController : MonoBehaviour
         if (!LevelEnded)
         {
             CameraYSpeed = Time.deltaTime;
-            switch (CameraPosition)
-            {
-                case CameraPositions.up:
-                    CameraYOffset = Mathf.MoveTowards(CameraYOffset, CameraMaxYOffset, CameraYSpeed);
-                    break;
-                case CameraPositions.middle:
-                    CameraYOffset = Mathf.MoveTowards(CameraYOffset, CameraMiddleYOffset, CameraYSpeed);
-                    break;
-                case CameraPositions.down:
-                    CameraYOffset = Mathf.MoveTowards(CameraYOffset, CameraMinYOffset, CameraYSpeed);
-                    break;
-            }
-            CameraYOffset = Mathf.Clamp(CameraYOffset, CameraMinYOffset, CameraMaxYOffset);
-
-            //Vector3 NewCameraPosition = PlayerTransform.position - Vector3.forward * CameraZOffset + Vector3.up * CameraYOffset;
-            //NewCameraPosition.x = PlayerTransform.position.x;
-            //transform.position = NewCameraPosition;
-            //transform.LookAt(PlayerTransform.position, PlayerTransform.up);
+            CameraYOffset = Mathf.MoveTowards(CameraYOffset, CameraMaxYOffset, CameraYSpeed);
         }
 
         transform.LookAt(PlayerTransform.position, Vector3.up);
@@ -65,21 +37,11 @@ public class CameraController : MonoBehaviour
         if (!LevelEnded)
         {
             Vector3 targetPos = PlayerTransform.position + (PlayerTransform.rotation * Distance);
-            //Vector3 NewCameraPosition = Vector3.SmoothDamp(transform.position, targetPos, ref CurrentVelocity, SmoothTime);
             float NewCameraYPosition = Mathf.SmoothDamp(transform.position.y, targetPos.y, ref CurrentVelocity.y, SmoothTime);
             float NewCameraZPosition = Mathf.SmoothDamp(transform.position.z, targetPos.z, ref CurrentVelocity.z, SmoothTime);
 
-
             transform.position = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y, NewCameraZPosition);
-            //transform.position = new Vector3(PlayerTransform.position.x, NewCameraPosition.y, NewCameraPosition.z);
-            //transform.LookAt(PlayerTransform.position, PlayerTransform.up);
         }
-    }
-
-    public void SetCameraNextPosition(CameraPositions newPos)
-    {
-        CameraLastposition = CameraPosition;
-        CameraPosition = newPos;
     }
 
     public void StopMovingCamera()
