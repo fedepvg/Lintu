9cd244d171a65fcd931a498a94a4a09f0958707a
fedepvg <federicopvg@gmail.com>
2019-12-02 12:43:22 -0300

    ui settings script created

---
+++ Lintu/Assets/Scripts/UI/UISettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISettings : MonoBehaviour
+{
+    public Toggle InvertedYToggle;
+    public Slider VolumeSlider;
+
+    bool CurrentToggleValue;
+
+    void Start()
+    {
+        InvertedYToggle.isOn = GameManager.Instance.InvertedY;
+        CurrentToggleValue = InvertedYToggle.isOn;
+    }
+
+    void Update()
+    {
+        if (CurrentToggleValue != InvertedYToggle.isOn)
+        {
+            CurrentToggleValue = InvertedYToggle.isOn;
+            GameManager.Instance.InvertedY = CurrentToggleValue;
+        }
+    }
+}
