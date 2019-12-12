63b13e7bd4c098dc139d3dcdc58d6607f9ab135e
fedepvg <federicopvg@gmail.com>
2019-12-12 16:48:32 -0300

    endless with pooling

--- Lintu/Assets/Scripts/LevelChunkGenerator.cs
+++ Lintu/Assets/Scripts/LevelChunkGenerator.cs
@@ -8,7 +8,7 @@ public class LevelChunkGenerator : MonoBehaviour
     public GameObject LevelChunkPrefab;
     public GameObject FirstObstaclePosition;
     public GameObject ChunkFinishPosition;
-    public static bool FirstChunk = true;
+    public bool FirstChunk;
     public Transform ObstaclesParent;
 
     public List<GameObject> ObstaclesList;
@@ -22,7 +22,6 @@ public class LevelChunkGenerator : MonoBehaviour
         if (FirstChunk)
         {
             GenerateNextChunk();
-            FirstChunk = false;
         }
         else
         {
@@ -38,14 +37,16 @@ public class LevelChunkGenerator : MonoBehaviour
         while (generating)
         {
             int rand = Random.Range(0, ObstaclesPrefabs.Count);
-            GameObject go = Instantiate(ObstaclesPrefabs[rand], NextObstaclePosition, Quaternion.identity);
+            //GameObject go = Instantiate(ObstaclesPrefabs[rand], NextObstaclePosition, Quaternion.identity);
+            GameObject go = ObjectPooler.Instance.GetPooledObject("Obstacle");
+            go.transform.position = NextObstaclePosition;
+            go.transform.rotation = Quaternion.identity;
             ObstaclesList.Add(go);
             
             PrevObstaclePosition = NextObstaclePosition;
             NextObstaclePosition += Vector3.forward * 200;
             if (NextObstaclePosition.z > ChunkFinishPosition.transform.position.z)
                 generating = false;
-            Debug.Log("zarlanga");
         }
 
          ReScaleObstacles();
@@ -63,16 +64,39 @@ public class LevelChunkGenerator : MonoBehaviour
 
     public void GenerateNextChunk()
     {
-        GameObject go = Instantiate(LevelChunkPrefab, ChunkFinishPosition.transform.position, LevelChunkPrefab.transform.rotation);
+        GameObject go = ObjectPooler.Instance.GetPooledObject("Environment");
+        go.transform.position = ChunkFinishPosition.transform.position;
+        go.transform.rotation = LevelChunkPrefab.transform.rotation;
+        //GameObject go = Instantiate(LevelChunkPrefab, ChunkFinishPosition.transform.position, LevelChunkPrefab.transform.rotation);
         NextChunk = go;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag=="Player")
+        if(other.tag == "Player")
         {
+            if (FirstChunk)
+                Destroy(gameObject, 1f);
+            else
+                StartCoroutine(DeactivateChunk(1f));
+            //Destroy(gameObject, 1f);
             NextChunk.GetComponent<LevelChunkGenerator>().GenerateNextChunk();
-            Destroy(gameObject, 1f);
         }
+    }
+
+    private void OnDisable()
+    {
+        foreach (GameObject go in ObstaclesList)
+        {
+            go.transform.SetParent(null);
+            go.SetActive(false);
+        }
+    }
+
+    IEnumerator DeactivateChunk(float t)
+    {
+        yield return new WaitForSeconds(t);
+
+        gameObject.SetActive(false);
     }
 }
---
+++ Lintu/Assets/Scripts/Utils/ObjectPooler.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObjectPoolItem
+{
+    public GameObject ObjectToPool;
+    public int AmountToPool;
+    public GameObject Parent;
+}
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler Instance;
+
+    public List<ObjectPoolItem> ItemsToPool;
+    public List<GameObject> PooledObjects;
+
+    public void Awake()
+    {
+        Instance = this;
+        PooledObjects = new List<GameObject>();
+
+        foreach (ObjectPoolItem item in ItemsToPool)
+        {
+            for (int i = 0; i < item.AmountToPool; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(item.ObjectToPool);
+                obj.SetActive(false);
+                if(item.Parent!=null)
+                    obj.transform.parent = item.Parent.transform;
+                obj.name = item.ObjectToPool.name + i;
+                PooledObjects.Add(obj);
+            }
+        }
+    }
+
+    public void Start()
+    {
+        //PooledObjects = new List<GameObject>();
+        //
+        //foreach (ObjectPoolItem item in ItemsToPool)
+        //{
+        //    for (int i = 0; i < item.AmountToPool; i++)
+        //    {
+        //        GameObject obj = (GameObject)Instantiate(item.ObjectToPool);
+        //        obj.SetActive(false);
+        //        obj.transform.parent = item.Parent.transform;
+        //        obj.name = item.ObjectToPool.name + i;
+        //        PooledObjects.Add(obj);
+        //    }
+        //}
+    }
+
+    public GameObject GetPooledObject(string tag)
+    {
+        for (int i = 0; i < PooledObjects.Count; i++)
+        {
+            if (!PooledObjects[i].activeInHierarchy && PooledObjects[i].tag == tag)
+            {
+                PooledObjects[i].SetActive(true);
+                return PooledObjects[i];
+            }
+        }
+        return null;
+    }
+
+    public void DeactivateObject(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+}
