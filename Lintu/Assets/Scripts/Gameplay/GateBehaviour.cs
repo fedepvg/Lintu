f4beca3e3779cd89ca100d9097dd1ac743220906
fedepvg <federicopvg@gmail.com>
2019-12-15 18:36:04 -0300

    dynamic gate improved

--- Lintu/Assets/Scripts/Gameplay/GateBehaviour.cs
+++ Lintu/Assets/Scripts/Gameplay/GateBehaviour.cs
@@ -11,12 +11,17 @@ public class GateBehaviour : MonoBehaviour
     public List<GameObject> SecondDiagonalGears;
     public float GateMovementTime;
     public float GearRotationSpeed;
+    public float StopTime;
 
     float TopYPosition;
     float BottomYPosition;
-    float GearDirectionSign = 1;
+    int GearDirectionSign = 1;
     float GateMovementTimer;
     float PrevFrameTime;
+    float StopTimer;
+    float PingPongTimer;
+    int PrevSign;
+    bool Stop;
 
     // Start is called before the first frame update
     void Start()
@@ -33,31 +38,50 @@ public class GateBehaviour : MonoBehaviour
         float CenterYPos;
         float RightYPos;
 
-        GateMovementTimer = Mathf.PingPong(Time.time, GateMovementTime) / GateMovementTime;
 
-        LeftYPos = Mathf.Lerp(BottomYPosition, TopYPosition, GateMovementTimer);
-        CenterYPos = Mathf.Lerp(TopYPosition, BottomYPosition, GateMovementTimer);
-        RightYPos = Mathf.Lerp(BottomYPosition, TopYPosition, GateMovementTimer);
-
-        LeftGate.transform.localPosition = new Vector3(LeftGate.transform.localPosition.x, LeftYPos, LeftGate.transform.localPosition.z);
-        CenterGate.transform.localPosition = new Vector3(CenterGate.transform.localPosition.x, CenterYPos, CenterGate.transform.localPosition.z);
-        RightGate.transform.localPosition = new Vector3(RightGate.transform.localPosition.x, RightYPos, RightGate.transform.localPosition.z);
-
-        if (PrevFrameTime <= GateMovementTimer)
-            GearDirectionSign = 1;
-        else if (PrevFrameTime >=GateMovementTimer)
-            GearDirectionSign = -1;
-
-        PrevFrameTime = GateMovementTimer;
-
-        for (int i = 0; i < FirstDiagonalGears.Count; i++)
+        if (!Stop)
         {
-            FirstDiagonalGears[i].transform.Rotate(Vector3.right * GearRotationSpeed * GearDirectionSign * Time.deltaTime);
+            PingPongTimer += Time.deltaTime;
+            GateMovementTimer = Mathf.PingPong(PingPongTimer, GateMovementTime) / GateMovementTime;
+
+            LeftYPos = Mathf.Lerp(BottomYPosition, TopYPosition, GateMovementTimer);
+            CenterYPos = Mathf.Lerp(TopYPosition, BottomYPosition, GateMovementTimer);
+            RightYPos = Mathf.Lerp(BottomYPosition, TopYPosition, GateMovementTimer);
+
+            LeftGate.transform.localPosition = new Vector3(LeftGate.transform.localPosition.x, LeftYPos, LeftGate.transform.localPosition.z);
+            CenterGate.transform.localPosition = new Vector3(CenterGate.transform.localPosition.x, CenterYPos, CenterGate.transform.localPosition.z);
+            RightGate.transform.localPosition = new Vector3(RightGate.transform.localPosition.x, RightYPos, RightGate.transform.localPosition.z);
+
+            if (PrevFrameTime < GateMovementTimer)
+                GearDirectionSign = 1;
+            else if (PrevFrameTime > GateMovementTimer)
+                GearDirectionSign = -1;
+
+            PrevFrameTime = GateMovementTimer;
+
+            if (PrevSign != GearDirectionSign)
+                Stop = true;
+            PrevSign = GearDirectionSign;
+
+            for (int i = 0; i < FirstDiagonalGears.Count; i++)
+            {
+                FirstDiagonalGears[i].transform.Rotate(Vector3.right * GearRotationSpeed * GearDirectionSign * Time.deltaTime);
+            }
+
+            for (int i = 0; i < SecondDiagonalGears.Count; i++)
+            {
+                SecondDiagonalGears[i].transform.Rotate(Vector3.right * -GearRotationSpeed * GearDirectionSign * Time.deltaTime);
+            }
+        }
+        else
+        {
+            StopTimer += Time.deltaTime;
+            if (StopTimer >= StopTime)
+            {
+                Stop = false;
+                StopTimer = 0;
+            }
         }
 
-        for (int i = 0; i < SecondDiagonalGears.Count; i++)
-        {
-            SecondDiagonalGears[i].transform.Rotate(Vector3.right * -GearRotationSpeed * GearDirectionSign * Time.deltaTime);
-        }
     }
 }
