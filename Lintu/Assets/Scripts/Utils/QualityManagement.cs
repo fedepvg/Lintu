0674d4f584025088eed4e2697d8890b7b5d92c93
fedepvg <federicopvg@gmail.com>
2019-12-16 11:24:04 -0300

    quality management created

---
+++ Lintu/Assets/Scripts/Utils/QualityManagement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Tier
+{
+    public int VRam;
+    public int Ram;
+    public int ProcessorCount;
+}
+
+public class QualityManagement : MonoBehaviour
+{
+    public List<Tier> TargetGraphicTiers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
