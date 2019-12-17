ebe347cbc5737454332bf4720d26aad2f66f7f8c
fedepvg <federicopvg@gmail.com>
2019-12-17 10:57:20 -0300

    best time in mode selection

---
+++ Lintu/Assets/Scripts/UI/UIModeSelection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIModeSelection : MonoBehaviour
+{
+    public GameObject BestTimeGO;
+    public TextMeshProUGUI BestTimeText;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("BestTime"))
+        {
+            float bestTime = PlayerPrefs.GetFloat("BestTime");
+            int minutes = TimeToMinutes(bestTime);
+            int seconds = TimeToSeconds(bestTime);
+            BestTimeText.text= string.Format("{0:0}:{1:00}", minutes, seconds);
+        }
+        else
+            BestTimeGO.SetActive(false);
+    }
+   
+    int TimeToSeconds(float t)
+    {
+        return (int)(t % 60);
+    }
+
+    int TimeToMinutes(float t)
+    {
+        return (int)(t / 60);
+    }
+}
